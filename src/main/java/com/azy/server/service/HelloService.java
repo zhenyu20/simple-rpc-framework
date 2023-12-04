08f79784a2e05959c45cc27cb47c69847a5c73ff
zhenyu20 <azy20804@gmail>
2023-12-04 21:12:59 +0800

    simple rpc

---
+++ src/main/java/com/azy/server/service/HelloService.java
@@ -0,0 +1,5 @@
+package com.azy.server.service;
+
+public interface HelloService {
+    String sayHello(String name);
+}
---
+++ src/main/java/com/azy/server/service/HelloServiceImpl.java
@@ -0,0 +1,10 @@
+package com.azy.server.service;
+
+
+public class HelloServiceImpl implements HelloService {
+    @Override
+    public String sayHello(String msg) {
+//        int i = 1 / 0;
+        return "你好, " + msg;
+    }
+}
